43ab849b388fa0f21116a4da6294a591c6725aa6
rankr6 <125333713+rankr6@users.noreply.github.com>
2024-03-19 10:37:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "./context/theme.tsx";
+import * as Sentry from "@sentry/react";
+
+
+Sentry.init({
+  dsn: "https://1c0af9c9eb1977ed396ea1aff09ac5bd@o4506935832346624.ingest.us.sentry.io/4506935834574848",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider>
     <App />
   </ThemeProvider>,
-)
+) 

530761fa8279b4467aeab20de19b46bd5ec8e656
rankr6 <125333713+rankr6@users.noreply.github.com>
2024-03-19 10:43:41 +0530

    Update ArticleDetails.tsx

--- src/pages/NewsArticle/ArticleDetails.tsx
+++ src/pages/NewsArticle/ArticleDetails.tsx
@@ -91,7 +91,9 @@ const ArticleDetails = () => {
                                     >
                                         Close
                                     </button>
-                                    <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+                                    <button onClick={() => methodDoesNotExist()}
+                                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                        >Break the world</button>;
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
